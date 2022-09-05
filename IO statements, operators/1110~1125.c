8134cc09950c32cb715e4292bffe65f280272e3b
woong <103757515+woong12@users.noreply.github.com>
2022-09-05 22:39:51 +0900

    Add files via upload

--- IO statements, operators/1110~1125.c
+++ IO statements, operators/1110~1125.c
@@ -10,55 +10,7 @@ int main() {
 
 ----------*/
 
-/* 1002
-
-
-
-----------*/
-
-/* 1003
-
-
-
-----------*/
-
-/* 1004
-
-
-
-----------*/
-
-/* 1005
-
-
-
-----------*/
-
-/* 1006
-
-
-
-----------*/
-
-/* 1007
-
-
-
-----------*/
-
-/* 1008
-
-
-
-----------*/
-
-/* 1010
-
-
-
-----------*/
-
-
+/* 1111
 
 #include <stdio.h>
 
@@ -66,4 +18,171 @@ int main() {
     int a;
     scanf("%d", &a);
     printf("%d", a);
+    printf("%%");
 }
+
+----------*/
+
+/* 1112
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a, b);
+}
+
+----------*/
+
+/* 1113
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d", b, a);
+}
+
+----------*/
+
+/* 1114
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+}
+
+----------*/
+
+/* 1115
+
+#include <stdio.h>
+
+int main() {
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a + b);
+}
+
+----------*/
+
+/* 1116
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d+%d=%d\n", a, b, a + b);
+    printf("%d-%d=%d\n", a, b, a - b);
+    printf("%d*%d=%d\n", a, b, a * b);
+    printf("%d/%d=%d\n", a, b, a / b);
+}
+
+----------*/
+
+/* 1117
+
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    scanf("%f %f", &a, &b);
+    printf("%.2f", a * b);
+}
+
+----------*/
+
+/* 1118
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%.1f", (float)(a * b)/2);
+}
+
+
+----------*/
+
+/* 1119
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d", a * 24);
+}
+
+----------*/
+
+/* 1120
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%.2f", (float)(a+b+c)/3.0);
+}
+
+----------*/
+
+/* 1121
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a%b);
+}
+
+----------*/
+
+/* 1122
+
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d %d", a/60, a%60);
+}
+
+----------*/
+
+/* 1123
+
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%.3f", (float)((9.0*a)/5+32));
+}
+
+----------*/
+
+/* 1125
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%o %X", a, a);
+}
+
+----------*/
+
+
