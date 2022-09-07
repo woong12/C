1bcccb1593cbd49abd21b5467f463c0e301c3921
woong <103757515+woong12@users.noreply.github.com>
2022-09-07 21:20:15 +0900

    Add files via upload

--- if~else/1151~1180.c
+++ if~else/1151~1180.c
@@ -1,59 +1,163 @@
 /* 1151
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a < 10) {
+        printf("small");
+    }
+}
 
 ----------*/
 
 /* 1152
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a < 10) {
+        printf("small");
+    } else {
+        printf("big");
+    }
+}
 
 ----------*/
 
 /* 1153
 
+#include <stdio.h>
 
+int main() {
+    int a, b;
+    scanf("%d %d", &a ,&b);
+    if(a > b) {
+        printf(">");
+    } else if(b > a) {
+        printf("<");
+    } else if(a == b) {
+        printf("=");
+    }
+}
 
 ----------*/
 
 /* 1154
 
+#include <stdio.h>
 
+int main() {
+    int a, b;
+    scanf("%d %d", &a ,&b);
+    if (a > b) {
+        printf("%d", a-b);
+    } else if(b > a) {
+        printf("%d", b-a);
+    } else if(a == b) {
+        printf("%d", a-b);
+    }
+}
 
 ----------*/
 /* 1155
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a%7==0) {
+        printf("multiple");
+    } else {
+        printf("not multiple");
+    }
+}
 
 ----------*/
 
 /* 1156
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a % 2 == 1) {
+        printf("odd");
+    } else {
+        printf("even");
+    }
+}
 
 ----------*/
 
 /* 1157
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a >= 50 && a < 60) {
+        printf("win");
+    } else {
+        printf("lose");
+    }
+}
 
 ----------*/
 
 /* 1158
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if((a >= 30 && a <= 40) || (a >= 60 && a <= 70)) {
+        printf("win"); 
+    } else {
+        printf("lose");
+    }
+}
 
 ----------*/
 
 /* 1159
 
+#include <stdio.h>
 
+int main() {
+    int a;
+    scanf("%d", &a);
+    if((a >= 50 && a <= 70) || (a % 6 == 0)) {
+        printf("win"); 
+    } else {
+        printf("lose");
+    }
+}
 
 ----------*/
 
 /* 1160
 
+#include <stdio.h>
 
+int main() {
+
+    int a;
+    scanf("%d", &a);
+    
+    if(a % 2 == 1) { 
+        printf("oh my god");
+    }else{
+        printf("enjoy");
+    }
+}
 
 ----------*/
 
@@ -146,6 +250,13 @@
 #include <stdio.h>
 
 int main() {
-    
-}
 
+    int a;
+    scanf("%d", &a);
+    
+    if(a % 2 == 1) { 
+        printf("oh my god");
+    }else{
+        printf("enjoy");
+    }
+}
